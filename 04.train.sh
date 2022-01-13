34688500e249a884645927a6535fbf97be001003
Joe Hsiao <joe.phsiao@gmail.com>
2022-01-13 08:17:59 -0500

    Save console output into log.log.

--- 04.train.sh
+++ 04.train.sh
@@ -4,12 +4,13 @@ data_bin_dir=data/news-commentary-v15.en-zh/bin
 chkp_dir=model/checkpoints
 tensorboard_dir=loggin
 
-fairseq-train $data_bin_dir --arch transformer \
+fairseq-train $data_bin_dir \
+    --arch transformer \
 	--source-lang $src --target-lang $tgt  \
     --optimizer adam  --lr 0.001 --adam-betas '(0.9, 0.98)' \
     --lr-scheduler inverse_sqrt --max-tokens 2048  --dropout 0.3 \
-    --criterion label_smoothed_cross_entropy  --label-smoothing 0.1 \
-    --max-update 200000  --warmup-updates 4000 --warmup-init-lr '1e-07' \
-    --keep-last-epochs 10 --num-workers 8 \
+    --criterion label_smoothed_cross_entropy \
+    --max-update 200000 \
+    --keep-last-epochs 10 \
     --tensorboard-logdir $tensorboard_dir \
-	--save-dir $chkp_dir
+	--save-dir $chkp_dir | tee log.log
